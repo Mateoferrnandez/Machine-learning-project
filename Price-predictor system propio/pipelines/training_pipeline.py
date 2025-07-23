dfa9102418a211d1aacc6ecca32a4750de0d30e8
Mateoferrnandez <126991426+Mateoferrnandez@users.noreply.github.com>
2025-07-23 14:05:46 -0500

    commit

---
+++ Price-predictor system propio/pipelines/training_pipeline.py
@@ -0,0 +1,26 @@
+from steps.data_ingestion_step import data_ingestion_step
+#from steps.data_splitter_step import data_splitter_step
+#from steps.feature_engineering_step import feature_engineering_step
+from steps.handle_missing_values_step import handle_missing_values_step
+#from steps.model_building_step import model_building_step
+#from steps.model_evaluator_step import model_evaluator_step
+#from steps.outlier_detection_step import outlier_detection_step
+from zenml import Model, pipeline, step
+
+@pipeline(
+    model=Model(
+        #The name uniquely identifies this model
+        name="sells_pridector"
+        ),
+)
+def ml_pipeline():
+    """Define an end-to-end machine learning pipeline."""
+    #Data Ingestion Step
+    raw_data = data_ingestion_step("D:\Documentos\Github\Machine-learning-end-to-end-project\Price-predictor system propio\data\Datosmodelo.csv")
+
+# Handling Missing Values Step
+    filled_data = handle_missing_values_step(raw_data)
+
+if __name__ == "__main__":
+    # Running the pipeline
+    run = ml_pipeline()
---
+++ Price-predictor system propio/src/ingest_data.py
@@ -0,0 +1,81 @@
+import os
+import zipfile
+from abc import ABC, abstractmethod
+
+import pandas as pd
+
+#Define an abstract method class for data ingestor
+class DataIngestor(ABC):
+    @abstractmethod
+    def ingest(self, file_path:str) -> pd.DataFrame:
+        """ Abstract method to ingest data from a given file """
+        pass
+
+#Implement a concret class for zip ingestion
+class ZipDataIngestor(DataIngestor):
+    def ingest(self, file_path:str) -> pd.DataFrame:
+        """ Extracts a zip file and returns the content as a pandas dataframe """
+        #Ensure the file is a .zip
+        if not file_path.endswith(".zip"):
+            raise ValueError("The provided file is not a .zip file.")
+
+        # Extract the zip file
+        with zipfile.ZipFile(file_path,"r") as zip_ref:
+            zip_ref.extractall("extracted_data")
+        # Find the extracted CSV file (assuming theres one inside the zip) 
+        extracted_files=os.listdir("extracted_data")
+        csv_files=[f for f in extracted_files if f.endswith(".csv")]
+
+        if len(csv_files) == 0:
+            raise FileNotFoundError("No CSV file found in the extracted data.")
+        if len(csv_files) > 1:
+            raise ValueError("Multiple CSV files found. Please specify which one to use.")
+        
+        #read the csv into a dataframe
+        csv_file_path=os.path.join("extracted_data",csv_files[0])
+        df=pd.read_csv(csv_file_path)
+        #Return the Dataframe
+        return df
+    
+class CsvDataIngestor(DataIngestor):
+    def ingest(self, file_path:str) -> pd.DataFrame:
+
+        #Ensure the file is a .csv
+        if not file_path.endswith(".csv"):
+            raise ValueError("The provided file is not a .csv file")
+        #read the csv into a dataframe
+        df=pd.read_csv(file_path)
+        
+        return df
+
+
+#Implement a factory to create data ingestors
+class DataIngestorFactory:
+    @staticmethod
+    def get_data_ingestor(file_extension:str) -> DataIngestor:
+        """Returns the appropiate DataIngestor base on the file extension."""
+        if file_extension == ".zip":
+            return ZipDataIngestor()
+        elif file_extension ==".csv":
+            return CsvDataIngestor()
+        else:
+            raise ValueError(f" No ingestor available for file extension:{file_extension}")
+            
+#Example of usage
+
+if __name__ == "__main__":
+    # # specify the file path
+    #file path = example.zip
+
+    ##Determine the file extension
+    #file_extension = os.path.splitext(filepath)[1]
+
+    ##Get the appropiate DataIngestor
+    #data_ingestor=DataIngestorFactory.get_data_ingestor(file_extension)
+
+    ## Ingest the data and load it into a dataframe
+    # df = data_ingestor.ingest(file_path)
+
+    ## Now df contains the dataframe from the extracted csv
+    # print(df.head())
+    pass
