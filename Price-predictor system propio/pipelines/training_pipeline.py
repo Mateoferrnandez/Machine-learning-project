358c2905f8453b51cd7b17dff7603a3e8f10e82d
Mateoferrnandez <126991426+Mateoferrnandez@users.noreply.github.com>
2025-07-24 19:39:23 -0500

    feature_enginner

--- Price-predictor system propio/pipelines/training_pipeline.py
+++ Price-predictor system propio/pipelines/training_pipeline.py
@@ -1,6 +1,6 @@
 from steps.data_ingestion_step import data_ingestion_step
 #from steps.data_splitter_step import data_splitter_step
-#from steps.feature_engineering_step import feature_engineering_step
+from steps.feature_engineering_step import feature_engineering_step
 from steps.handle_missing_values_step import handle_missing_values_step
 #from steps.model_building_step import model_building_step
 #from steps.model_evaluator_step import model_evaluator_step
@@ -18,8 +18,16 @@ def ml_pipeline():
     #Data Ingestion Step
     raw_data = data_ingestion_step("D:\Documentos\Github\Machine-learning-end-to-end-project\Price-predictor system propio\data\Datosmodelo.csv")
 
-# Handling Missing Values Step
+    #   Handling Missing Values Step
     filled_data = handle_missing_values_step(raw_data)
+
+    # Feature Engineering Step
+    engineered_data = feature_engineering_step(filled_data,strategy="onehot_ncoding",features=[['CODIGO_DEL_PRODUCTO','VENTA', 'VENTA_ZONA_101',
+       'VENTA_ZONA_102', 'VENTA_ZONA_103', 'VENTA_ZONA_104', 'VENTA_ZONA_107',
+       'VENTA_ZONA_109', 'VENTA_ZONA_110', 'VENTA_ZONA_111', 'VENTA_ZONA_112',
+       'VENTA_ZONA_115', 'VENTA_ZONA_116', 'VENTA_ZONA_119','TALLA','NOMB_SUBGRUPO','CAMPANA']]
+    )
+
 
 if __name__ == "__main__":
     # Running the pipeline
