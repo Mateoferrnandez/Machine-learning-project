90b9c655c90bb86b42eece934273a79c93c09f9e
Mateoferrnandez <126991426+Mateoferrnandez@users.noreply.github.com>
2025-08-08 18:30:52 -0500

    deployment_pipeline

---
+++ steps/dynamic_importer.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from zenml import step
+
+@step
+def dynamic_importer() -> str:
+    """Dynamically imports data for testing out the model."""
+    # Here, we simulate importing or generating some data.
+    # In a real-world scenario, this could be an API call, database query, or loading from a file.
+    data = {}
+    df = pd.DataFrame(data)
+
+    # Convert the DataFrame to a JSON string
+    json_data = df.to_json(orient="split")
+
+    return json_data
